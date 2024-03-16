2a1dccf2cf75c2db11483323a2c841234cd5b638
AhmdBdran1 <ahmdbdran@h-Air-sl-user.Dlink>
2024-03-16 13:18:07 +0200

    try to fix

--- infra/browser_wraper.py
+++ infra/browser_wraper.py
@@ -27,7 +27,8 @@ class BrowserWrapper:
         option.add_argument('--disable-dev-shm-usage')
         option.add_argument('--window-size=1920x1080')
         if grid:
-            print(option)
+            print('ala ala')
+
             driver = webdriver.Remote(command_executor=hub_url, options=option)
             driver.get(url)
             driver.maximize_window()
